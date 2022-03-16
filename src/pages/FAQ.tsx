1daa1390807c7ca6bb93449fd736efed4691b196
Jasper Swart <jasper.g.swart@gmail.com>
2022-03-16 17:21:52 +0100

    added daq content

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Heading, themeSpacing } from "@amsterdam/asc-ui";
+import { Accordion, Heading, Paragraph, themeSpacing } from "@amsterdam/asc-ui";
 
 const StyledDiv = styled.div`
   margin-top: ${themeSpacing(10)};
@@ -10,12 +10,24 @@ const StyledHeading = styled(Heading)`
   margin-bottom: ${themeSpacing(5)};
 `;
 
+const StyledAccordion = styled(Accordion)`
+  margin-top: ${themeSpacing(3)};
+`;
+
 const FAQ = () => {
   return (
     <>
       <StyledDiv data-testid="faq">
-        <StyledHeading>FAQ</StyledHeading>
+        <StyledHeading>Veelgestelde vragen</StyledHeading>
 
+        <StyledAccordion id="a1" title="Wat is het HIOR Amsterdam?">
+          <Paragraph gutterBottom={0}>
+            Het Handboek Inrichting Openbare Ruimte Amsterdam (HIOR Amsterdam) is een digitaal platform waarop al het
+            bestaande beleid voor de inrichting van de Amsterdamse openbare ruimte op hoofdlijnen is samengevat. Met het
+            HIOR Amsterdam worden meer dan 30 beleidsproducten integraal ontsloten. Het HIOR Amsterdam heeft op zichzelf
+            géén bestuurlijke status. De bestuurlijke status is verbonden aan de achterliggende beleidsdocumenten.
+          </Paragraph>
+        </StyledAccordion>
       </StyledDiv>
     </>
   );
