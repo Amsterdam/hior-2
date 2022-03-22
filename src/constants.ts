873f9687e894b021686a693844518e83e90a933c
Jasper Swart <jasper.g.swart@gmail.com>
2022-03-22 15:45:17 +0100

    updated urls

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,8 @@
-export const API_URL = `https://${
+export const HIOR_ITEMS_URL = `https://${
   process.env.NODE_ENV === "development" ? "acc." : ""
-}api.data.amsterdam.nl/vsd/hior_attributes/?page=1&page_size=100000&format=json`;
+}api.data.amsterdam.nl/vsd/hior_items/?page=1&page_size=100000&format=json`;
+
+export const HIOR_PROPERTIES_URL = `https://${
+  process.env.NODE_ENV === "development" ? "acc." : ""
+}api.data.amsterdam.nl/vsd/hior_properties/?page=1&page_size=100000&format=json`;
+
