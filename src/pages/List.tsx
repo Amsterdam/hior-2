2e8017ef5267dda2651cd922d76947ad2511d2ec
Jasper Swart <jasper.g.swart@gmail.com>
2022-03-22 18:14:58 +0100

    List part 2

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -4,7 +4,6 @@ import { Accordion, Column, Heading, Paragraph, Row, themeSpacing } from "@amste
 import useDataFetching from "../hooks/useDataFetching";
 import { HIOR_ITEMS_URL, HIOR_PROPERTIES_URL } from "../constants";
 import { getByUri } from "../services/api";
-
 // import { Apicall } from "../types";
 
 const StyledDiv = styled.div`
@@ -26,8 +25,9 @@ const StyledAccordion = styled(Accordion)`
 
 const List = () => {
   const [attributes, setAttributes] = useState<any[]>([]);
+  const [allItems, setAllItems] = useState<any[]>([]);
   const { results, fetchData } = useDataFetching();
-  console.log("results", results, attributes);
+  console.log("allItems", allItems);
 
   const getAttributes = async () => {
     const attrs = await getByUri(HIOR_PROPERTIES_URL);
@@ -47,18 +47,24 @@ const List = () => {
       return;
     }
 
-    console.log("results loaded");
     // @ts-ignore
     const items = results.results.map((i) => {
       // @ts-ignore
       const found = attributes.results.filter((a) => i.id === a.item_id);
-      console.log("found", found);
+      const newAttr = {};
+      // @ts-ignore
+      found.map((attr) => {
+        // @ts-ignore
+        newAttr[attr.name.toLowerCase()] = attr.value;
+      });
 
       return {
         ...i,
+        ...newAttr,
       };
     });
-    console.log("items", items);
+
+    setAllItems(items);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [results, attributes]);
