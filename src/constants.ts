1f4310ef78bb67e982de8c216ebb186b57848ce4
Jasper Swart <jasper.g.swart@gmail.com>
2022-03-23 14:59:05 +0100

    added images

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+export const IMAGE_URL =  "https://131f4363709c46b89a6ba5bc764b38b9.objectstore.eu/hior/Afbeeldingen/";
+
 export const HIOR_ITEMS_URL = `https://${
   process.env.NODE_ENV === "development" ? "acc." : ""
 }api.data.amsterdam.nl/vsd/hior_items/?page=1&page_size=100000&format=json`;
@@ -9,4 +11,5 @@ export const HIOR_PROPERTIES_URL = `https://${
 export const HIOR_ATTRIBUTES_URL = `https://${
   process.env.NODE_ENV === "development" ? "acc." : ""
 }api.data.amsterdam.nl/vsd/hior_attributes/?page=1&page_size=100000&format=json`;
+
 
