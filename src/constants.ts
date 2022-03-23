f60597d810758958975ead50cafc08768529bbee
Jasper Swart <jasper.g.swart@gmail.com>
2022-03-23 12:45:51 +0100

    added props url

--- src/constants.ts
+++ src/constants.ts
@@ -6,3 +6,7 @@ export const HIOR_PROPERTIES_URL = `https://${
   process.env.NODE_ENV === "development" ? "acc." : ""
 }api.data.amsterdam.nl/vsd/hior_properties/?page=1&page_size=100000&format=json`;
 
+export const HIOR_ATTRIBUTES_URL = `https://${
+  process.env.NODE_ENV === "development" ? "acc." : ""
+}api.data.amsterdam.nl/vsd/hior_attributes/?page=1&page_size=100000&format=json`;
+
