b9aff5107841842e0e72f532b4d2044a714b3071
Jasper Swart <jasper.g.swart@gmail.com>
2022-03-29 15:34:32 +0200

    added metadata to constants

--- src/constants.ts
+++ src/constants.ts
@@ -14,4 +14,9 @@ export const HIOR_ATTRIBUTES_URL = `https://${
   process.env.NODE_ENV === "development" ? "acc." : ""
 }api.data.amsterdam.nl/vsd/hior_attributes/?page=1&page_size=100000&format=json`;
 
+export const HIOR_METADATA_URL = `https://${
+  process.env.NODE_ENV === "development" ? "acc." : ""
+}api.data.amsterdam.nl/vsd/hior_metadata/?page=1&page_size=100000&format=json`;
+
+
 
