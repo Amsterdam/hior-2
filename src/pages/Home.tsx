164bd846f157d424333c7a0d6100814a10374e39
Jasper Swart <jasper.g.swart@gmail.com>
2022-03-29 19:32:09 +0200

    removed parent p

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ import {
   TableCell,
   TableContainer,
   TableRow,
-  themeSpacing
+  themeSpacing,
 } from "@amsterdam/asc-ui";
 
 const StyledDiv = styled.div`
@@ -68,66 +68,84 @@ const Home = () => {
             <br />
             <br />
             <StyledAccordion id="a1" title="Niveau">
-              <Paragraph gutterBottom={0}>
-                <TableContainer>
-                  <Table>
-                    <TableBody>
-                      <TableRow>
-                        <StyledTablecell>Strategisch</StyledTablecell>
-                        <StyledTablecell>
-                          Dit beleid is strategisch, gericht op het bereiken van een overkoepelend doel. Het geeft
-                          richting aan de ontwikkeling van de stad en komt vaak voort uit visies, programma’s en
-                          agenda&rsquo;s.
-                        </StyledTablecell>
-                      </TableRow>
-                      <TableRow>
-                        <StyledTablecell>Tactisch</StyledTablecell>
-                        <StyledTablecell>Dit beleid geeft op tactisch niveau aan wat er nodig is voor een goede, functionele en duurzame openbare ruimte. Het is een uitwerking van bovenliggend strategisch beleid.</StyledTablecell>
-                      </TableRow>
-                      <TableRow>
-                        <StyledTablecell>Operationeel</StyledTablecell>
-                        <StyledTablecell>Dit beleid geeft op operationeel niveau aan hoe de openbare ruimte exact moet worden ontworpen, ingericht of aangelegd.</StyledTablecell>
-                      </TableRow>
-                      <TableRow>
-                        <StyledTablecell>Proces</StyledTablecell>
-                        <StyledTablecell>Dit zijn procesafspraken die bijdragen aan een betere samenwerking en uiteindelijk een beter eindresultaat in de openbare ruimte.</StyledTablecell>
-                      </TableRow>
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-              </Paragraph>
+              <TableContainer>
+                <Table>
+                  <TableBody>
+                    <TableRow>
+                      <StyledTablecell>Strategisch</StyledTablecell>
+                      <StyledTablecell>
+                        Dit beleid is strategisch, gericht op het bereiken van een overkoepelend doel. Het geeft
+                        richting aan de ontwikkeling van de stad en komt vaak voort uit visies, programma’s en
+                        agenda&rsquo;s.
+                      </StyledTablecell>
+                    </TableRow>
+                    <TableRow>
+                      <StyledTablecell>Tactisch</StyledTablecell>
+                      <StyledTablecell>
+                        Dit beleid geeft op tactisch niveau aan wat er nodig is voor een goede, functionele en duurzame
+                        openbare ruimte. Het is een uitwerking van bovenliggend strategisch beleid.
+                      </StyledTablecell>
+                    </TableRow>
+                    <TableRow>
+                      <StyledTablecell>Operationeel</StyledTablecell>
+                      <StyledTablecell>
+                        Dit beleid geeft op operationeel niveau aan hoe de openbare ruimte exact moet worden ontworpen,
+                        ingericht of aangelegd.
+                      </StyledTablecell>
+                    </TableRow>
+                    <TableRow>
+                      <StyledTablecell>Proces</StyledTablecell>
+                      <StyledTablecell>
+                        Dit zijn procesafspraken die bijdragen aan een betere samenwerking en uiteindelijk een beter
+                        eindresultaat in de openbare ruimte.
+                      </StyledTablecell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
             </StyledAccordion>
             <StyledAccordion id="a2" title="Type">
-              <Paragraph gutterBottom={0}>
-                <Paragraph>
-                  Hiermee wordt aangegeven hoe het beleid getypeerd wordt:
-                </Paragraph>
-                <TableContainer>
-                  <Table>
-                    <TableBody>
-                      <TableRow>
-                        <StyledTablecell>Randvoorwaarde</StyledTablecell>
-                        <StyledTablecell>
-                          De openbare ruimte wordt conform deze bestuurlijk vastgestelde randvoorwaarden ingericht. Afwijken is alleen mogelijk als daar door het bestuur expliciet toe besloten wordt.
-                        </StyledTablecell>
-                      </TableRow>
-                      <TableRow>
-                        <StyledTablecell>Uitgangspunt</StyledTablecell>
-                        <StyledTablecell>Deze uitgangspunten komen voort uit vastgesteld beleid. Ze vormen de basis voor de inrichting van de Amsterdamse openbare ruimte. Uitgangspunten bieden veelal ruimte voor diverse invullingen op projectniveau. Soms kan onderbouwd van de uitgangspunten worden afgeweken, in afstemming met de opstellers van het beleid en het bestuur.</StyledTablecell>
-                      </TableRow>
-                      <TableRow>
-                        <StyledTablecell>Ambitie</StyledTablecell>
-                        <StyledTablecell>De door de gemeenteraad, het college of het stadsdeelbestuur vastgestelde ambities worden meegenomen bij het inrichten van de openbare ruimte. Niet alle ambities zijn binnen één straat, park of plein te realiseren. Op projectniveau worden hierin keuzes gemaakt.</StyledTablecell>
-                      </TableRow>
-                      <TableRow>
-                        <StyledTablecell>Advies</StyledTablecell>
-                        <StyledTablecell>Dit zijn zwaarwegende adviezen die volgen uit richtlijnen, handleidingen, handboeken, etc. die geen bestuurlijke status (meer) hebben. De adviezen zijn wel van belang zijn voor een goede inrichting van de openbare ruimte. Als er geen invulling aan het advies gegeven kan worden, wordt dit afgestemd met de betrokken ambtenaren of voorgelegd aan de daarvoor bestemde adviesorganen, zoals de Centrale Verkeerscommissie.</StyledTablecell>
-                      </TableRow>
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-  
-              </Paragraph>
+              <Paragraph>Hiermee wordt aangegeven hoe het beleid getypeerd wordt:</Paragraph>
+              <TableContainer>
+                <Table>
+                  <TableBody>
+                    <TableRow>
+                      <StyledTablecell>Randvoorwaarde</StyledTablecell>
+                      <StyledTablecell>
+                        De openbare ruimte wordt conform deze bestuurlijk vastgestelde randvoorwaarden ingericht.
+                        Afwijken is alleen mogelijk als daar door het bestuur expliciet toe besloten wordt.
+                      </StyledTablecell>
+                    </TableRow>
+                    <TableRow>
+                      <StyledTablecell>Uitgangspunt</StyledTablecell>
+                      <StyledTablecell>
+                        Deze uitgangspunten komen voort uit vastgesteld beleid. Ze vormen de basis voor de inrichting
+                        van de Amsterdamse openbare ruimte. Uitgangspunten bieden veelal ruimte voor diverse invullingen
+                        op projectniveau. Soms kan onderbouwd van de uitgangspunten worden afgeweken, in afstemming met
+                        de opstellers van het beleid en het bestuur.
+                      </StyledTablecell>
+                    </TableRow>
+                    <TableRow>
+                      <StyledTablecell>Ambitie</StyledTablecell>
+                      <StyledTablecell>
+                        De door de gemeenteraad, het college of het stadsdeelbestuur vastgestelde ambities worden
+                        meegenomen bij het inrichten van de openbare ruimte. Niet alle ambities zijn binnen één straat,
+                        park of plein te realiseren. Op projectniveau worden hierin keuzes gemaakt.
+                      </StyledTablecell>
+                    </TableRow>
+                    <TableRow>
+                      <StyledTablecell>Advies</StyledTablecell>
+                      <StyledTablecell>
+                        Dit zijn zwaarwegende adviezen die volgen uit richtlijnen, handleidingen, handboeken, etc. die
+                        geen bestuurlijke status (meer) hebben. De adviezen zijn wel van belang zijn voor een goede
+                        inrichting van de openbare ruimte. Als er geen invulling aan het advies gegeven kan worden,
+                        wordt dit afgestemd met de betrokken ambtenaren of voorgelegd aan de daarvoor bestemde
+                        adviesorganen, zoals de Centrale Verkeerscommissie.
+                      </StyledTablecell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
             </StyledAccordion>
           </StyledDiv>
         </Column>
