a53a8b17fd5a59396c365e34ea439ee3affbea13
Jasper Swart <jasper.g.swart@gmail.com>
2022-03-31 15:33:26 +0200

    fixed test

--- src/components/UpdatedDate.test.tsx
+++ src/components/UpdatedDate.test.tsx
@@ -1,27 +1,30 @@
 import { render, screen } from "@testing-library/react";
-// import axios from "axios";
+import axios from "axios";
 import { withTheme } from "../test/utils";
 import UpdatedDate from "./UpdatedDate";
 
-// jest.mock('axios');
+jest.mock('axios');
 
 describe("UpdatedDate", () => {
   afterEach(() => {
     jest.resetAllMocks();
   });
 
-  // const mockData = {
-  //   results: [{
-  //     value: "2022-03-11 00:00:00"
-  //   }]
-  // };
+  const mockData = {
+    results: [{
+      value: "2022-03-11 00:00:00"
+    }]
+  };
 
 
-  it("renders correctly", () => {
-    // axios.get.mockResolvedValueOnce({ data: mockData });
+  it("renders correctly", async () => {
+    //@ts-ignore
+    axios.get.mockResolvedValueOnce({ data: mockData });
 
     render(withTheme(<UpdatedDate />));
 
-    expect(screen.queryByTestId("updated-date")).toBeInTheDocument();
+    expect(await screen.findByTestId("updated-date")).toBeInTheDocument();
+
+    expect(await screen.getByTestId("updated-date")).toHaveTextContent("11-3-2022");
   });
 });
