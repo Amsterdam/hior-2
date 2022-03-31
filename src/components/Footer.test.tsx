629f781a29666137e25f15e2dad84895010273e3
Jasper Swart <jasper.g.swart@gmail.com>
2022-03-31 15:51:09 +0200

    fixed footer test

--- src/components/Footer.test.tsx
+++ src/components/Footer.test.tsx
@@ -2,10 +2,11 @@ import { render, screen } from "@testing-library/react";
 import { withTheme } from "../test/utils";
 import Footer from "./Footer";
 
+
 describe("Footer", () => {
-  it("renders correctly", () => {
+  it("renders correctly", async () => {
     render(withTheme(<Footer />));
 
-    expect(screen.queryByTestId("footer")).toBeInTheDocument();
+    expect(await screen.queryByTestId("footer")).toBeInTheDocument();
   });
 });
