d594bf3b529d0d3d30cba3dbe446092c5e0a9322
Jasper Swart <jasper.g.swart@gmail.com>
2022-04-04 12:50:57 +0200

    split test files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router";
 import { withTheme } from "./test/utils";
-import { ThemeProvider } from "@amsterdam/asc-ui";
 
 import App from "./App";
+
+jest.mock("./components/UpdatedDate", () => () => 'UpdatedDate');
 
 describe("App", () => {
   it("renders correctly", () => {
@@ -11,53 +11,5 @@ describe("App", () => {
 
     expect(screen.queryByTestId("header")).toBeInTheDocument();
     expect(screen.queryByTestId("footer")).toBeInTheDocument();
-  });
-
-  it("redirect from / to home is defined", () => {
-    render(
-      <ThemeProvider>
-        <MemoryRouter initialEntries={["/"]}>
-          <App />
-        </MemoryRouter>
-      </ThemeProvider>,
-    );
-
-    expect(screen.queryByTestId("home")).toBeInTheDocument();
-  });
-
-  it("route to home is defined", () => {
-    render(
-      <ThemeProvider>
-        <MemoryRouter initialEntries={["/home"]}>
-          <App />
-        </MemoryRouter>
-      </ThemeProvider>,
-    );
-
-    expect(screen.queryByTestId("home")).toBeInTheDocument();
-  });
-
-  it("route to search is defined", () => {
-    render(
-      <ThemeProvider>
-        <MemoryRouter initialEntries={["/list"]}>
-          <App />
-        </MemoryRouter>
-      </ThemeProvider>,
-    );
-
-    expect(screen.queryByTestId("list")).toBeInTheDocument();
-  });
-
-  it("route to faq is defined", () => {
-    render(
-      <ThemeProvider>
-        <MemoryRouter initialEntries={["/faq"]}>
-          <App />
-        </MemoryRouter>
-      </ThemeProvider>,
-    );
-
-    expect(screen.queryByTestId("faq")).toBeInTheDocument();
   });
 });
---
+++ src/Routes.test.tsx
@@ -0,0 +1,57 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+// import { withTheme } from "./test/utils";
+import { ThemeProvider } from "@amsterdam/asc-ui";
+
+import App from "./App";
+
+describe("App", () => {
+
+  it("redirect from / to home is defined", () => {
+    render(
+      <ThemeProvider>
+        <MemoryRouter initialEntries={["/"]}>
+          <App />
+        </MemoryRouter>
+      </ThemeProvider>,
+    );
+
+    expect(screen.queryByTestId("home")).toBeInTheDocument();
+  });
+
+  it("route to home is defined", () => {
+    render(
+      <ThemeProvider>
+        <MemoryRouter initialEntries={["/home"]}>
+          <App />
+        </MemoryRouter>
+      </ThemeProvider>,
+    );
+
+    expect(screen.queryByTestId("home")).toBeInTheDocument();
+  });
+
+  it("route to search is defined", () => {
+    render(
+      <ThemeProvider>
+        <MemoryRouter initialEntries={["/list"]}>
+          <App />
+        </MemoryRouter>
+      </ThemeProvider>,
+    );
+
+    expect(screen.queryByTestId("list")).toBeInTheDocument();
+  });
+
+  it("route to faq is defined", () => {
+    render(
+      <ThemeProvider>
+        <MemoryRouter initialEntries={["/faq"]}>
+          <App />
+        </MemoryRouter>
+      </ThemeProvider>,
+    );
+
+    expect(screen.queryByTestId("faq")).toBeInTheDocument();
+  });
+});
