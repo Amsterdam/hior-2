e907bafa9239eee22f73de0a2b271c8e0f7d24da
Jasper Swart <jasper.g.swart@gmail.com>
2022-04-04 13:57:27 +0200

    fixed faq test

--- src/pages/FAQ.test.tsx
+++ src/pages/FAQ.test.tsx
@@ -1,12 +1,49 @@
 import { render, screen } from "@testing-library/react";
+import axios from "axios";
 import { withTheme } from "../test/utils";
 import FAQ from "./FAQ";
 
-describe("FAQ", () => {
-  it("renders correctly", () => {
-    render(withTheme(<FAQ />));
+jest.mock("axios");
 
-    expect(screen.queryByTestId("faq")).toBeInTheDocument();
-    expect(screen.queryByText("Veelgestelde vragen")).toBeInTheDocument();
+describe("FAQ", () => {
+  const mockData = {
+    results: [
+      {
+        id: 1,
+        answer: "vraag 1",
+        question: "antwoord 1",
+      },
+      {
+        id: 2,
+        answer: "vraag 2",
+        question: "antwoord 2",
+      },
+      {
+        id: 3,
+        answer: "vraag 3",
+        question: "antwoord 3",
+      },
+    ],
+  };
+
+  it("renders correctly", async () => {
+    //@ts-ignore
+    axios.get.mockResolvedValueOnce({ data: mockData });
+
+    const { container } = render(withTheme(<FAQ />));
+
+    expect(await screen.queryByTestId("faq")).toBeInTheDocument();
+    expect(await screen.queryByText("Veelgestelde vragen")).toBeInTheDocument();
+
+    expect(await screen.queryByTestId("faq")).toBeInTheDocument();
+
+    expect(await container.querySelectorAll("BUTTON").length).toBe(3);
+
+    expect(await screen.queryByText("vraag 1")).toBeInTheDocument();
+    expect(await screen.queryByText("antwoord 1")).toBeInTheDocument();
+    expect(await screen.queryByText("vraag 2")).toBeInTheDocument();
+    expect(await screen.queryByText("antwoord 2")).toBeInTheDocument();
+    expect(await screen.queryByText("vraag 3")).toBeInTheDocument();
+    expect(await screen.queryByText("antwoord 3")).toBeInTheDocument();
   });
 });
