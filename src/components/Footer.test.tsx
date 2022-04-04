44aed6c480b980ded31420d046c233b207306e49
Jasper Swart <jasper.g.swart@gmail.com>
2022-04-04 12:10:35 +0200

    fixed test footer

--- src/components/Footer.test.tsx
+++ src/components/Footer.test.tsx
@@ -2,11 +2,12 @@ import { render, screen } from "@testing-library/react";
 import { withTheme } from "../test/utils";
 import Footer from "./Footer";
 
+jest.mock("./UpdatedDate", () => () => 'UpdatedDate');
 
 describe("Footer", () => {
-  it("renders correctly", async () => {
+  it("renders correctly", () => {
     render(withTheme(<Footer />));
 
-    expect(await screen.queryByTestId("footer")).toBeInTheDocument();
+    expect(screen.queryByTestId("footer")).toBeInTheDocument();
   });
 });
