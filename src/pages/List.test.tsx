3ab3be64263e62ffafb4238b341ac68423f11d98
Jasper Swart <jasper.g.swart@gmail.com>
2022-04-05 16:12:41 +0200

    removed debug

--- src/pages/List.test.tsx
+++ src/pages/List.test.tsx
@@ -39,10 +39,10 @@ describe("List", () => {
 
     // there should be 3 images
     expect(await container.querySelectorAll("IMG").length).toBe(3);
-    
+
     // there should be 1 document
     // expect(await screen.queryByText("Omgevingsvisie 2050 (2021)")).toBeInTheDocument();
 
-    await screen.debug();
+    // await screen.debug();
   });
 });
