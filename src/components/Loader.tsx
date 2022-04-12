f9f519139096037f116dbc00150887180985a5c2
Jasper Swart <jasper.g.swart@gmail.com>
2022-04-12 15:23:54 +0200

    added loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,22 @@
+import { Spinner, themeSpacing } from "@amsterdam/asc-ui";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  flex-wrap: nowrap;
+  padding: ${themeSpacing(6)};
+`;
+
+const Loader = () => {
+  return (
+    <StyledDiv>
+      <Spinner />
+    </StyledDiv>
+  );
+};
+
+export default Loader;
