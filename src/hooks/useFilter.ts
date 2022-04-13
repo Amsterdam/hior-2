f677ab0b76cc984e1da2265a25bbb4757f7ae393
Jasper Swart <jasper.g.swart@gmail.com>
2022-04-14 00:15:20 +0200

    added more type

--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -1,48 +1,56 @@
 import { useMemo } from "react";
-import { Filter } from "../types";
+import { Filter, HiorEnriched } from "../types";
 
-function useFilter(filter: Filter, data: any[]) {
+function useFilter(filter: Filter, data: HiorEnriched[]) {
   return useMemo(() => {
     // First filter by year to reduce the amount of data we need to work with.
-    let filteredData: any[] = data;
+    let filteredData: HiorEnriched[] = data;
 
     if (filter.source) {
-      //@ts-ignore
       filteredData = filteredData.filter((d: any) => {
         return d.source === filter.source;
       });
     }
 
     if (filter.level) {
-      //@ts-ignore
       filteredData = filteredData.filter((d: any) => {
         return d.level === filter.level;
       });
     }
 
     if (filter.theme) {
-      //@ts-ignore
       filteredData = filteredData.filter((d: any) => {
         return d.theme === filter.theme;
       });
     }
 
     if (filter.type) {
-      //@ts-ignore
       filteredData = filteredData.filter((d: any) => {
         return d.type === filter.type;
       });
     }
 
     if (filter.area) {
-      //@ts-ignore
       filteredData = filteredData.filter((d: any) => {
         return d.area === filter.area;
       });
     }
 
+    if (filter.query) {
+      const re = new RegExp(filter.query, "gim");
+      filteredData = filteredData.filter((d: any) => {
+        const found = d.text.match(re);
+        return found && found[0];
+      });
+
+      filteredData = filteredData.filter((d: any) => {
+        const found = d.description.match(re);
+        return found && found[0];
+      });
+    }
+
     return filteredData;
-  }, [data, filter.area, filter.level, filter.source, filter.theme, filter.type]);
+  }, [data, filter]);
 }
 
 export default useFilter;
