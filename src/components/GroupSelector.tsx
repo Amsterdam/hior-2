696289f7c329f27a20fb6ad3947abea804060d58
Jasper Swart <jasper.g.swart@gmail.com>
2022-04-13 13:59:13 +0200

    removed Loader from GroupSelector

--- src/components/GroupSelector.tsx
+++ src/components/GroupSelector.tsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
-import { Tabs, Tab, themeSpacing, Spinner } from "@amsterdam/asc-ui";
-import { useContext, useCallback, useState, useEffect } from "react";
-import Loader from "./Loader";
+import { Tabs, Tab, themeSpacing } from "@amsterdam/asc-ui";
+import { useContext, useCallback } from "react";
 import { FilterContext } from "../filter/FilterContext";
 import { actions } from "../filter/reducer";
 
@@ -37,8 +36,6 @@ const GroupSelector = ({ groups }) => {
     },
   ];
 
-  const [loading, setLoading] = useState<boolean>(true);
-
   const {
     //@ts-ignore
     state: { group },
@@ -56,18 +53,14 @@ const GroupSelector = ({ groups }) => {
     [dispatch],
   );
 
-  useEffect(() => {
-    setLoading(false);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [group]);
-
   return (
     <StyledDiv data-testid="group-selector">
       <Tabs label="" initialTab={group}>
         {allGroups.map((b: any) => (
           <StyledTab key={b.value} id={b.value} data-value={b.value} label={b.label} onClick={onClickGroup}>
-            {loading && <Loader />}
-            {!loading && groups[b.value].map((group: string) => <span key={group}>{group}, </span>)}
+            {groups[b.value].map((group: string) => (
+              <span key={group}>{group}, </span>
+            ))}
           </StyledTab>
         ))}
       </Tabs>
