c0c7b0fee12fb7ee11c887b4fe5f62d7077e6ad6
Jasper Swart <jasper.g.swart@gmail.com>
2022-04-14 11:41:13 +0200

    added types to useFilter

--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -1,49 +1,48 @@
 import { useMemo } from "react";
 import { Filter, HiorEnriched } from "../types";
 
-function useFilter(filter: Filter, data: HiorEnriched[]) {
+function useFilter(filter: Filter, data: HiorEnriched[]): HiorEnriched[] {
   return useMemo(() => {
-    // First filter by year to reduce the amount of data we need to work with.
     let filteredData: HiorEnriched[] = data;
 
     if (filter.source) {
-      filteredData = filteredData.filter((d: any) => {
+      filteredData = filteredData.filter((d: HiorEnriched) => {
         return d.source === filter.source;
       });
     }
 
     if (filter.level) {
-      filteredData = filteredData.filter((d: any) => {
+      filteredData = filteredData.filter((d: HiorEnriched) => {
         return d.level === filter.level;
       });
     }
 
     if (filter.theme) {
-      filteredData = filteredData.filter((d: any) => {
+      filteredData = filteredData.filter((d: HiorEnriched) => {
         return d.theme === filter.theme;
       });
     }
 
     if (filter.type) {
-      filteredData = filteredData.filter((d: any) => {
+      filteredData = filteredData.filter((d: HiorEnriched) => {
         return d.type === filter.type;
       });
     }
 
     if (filter.area) {
-      filteredData = filteredData.filter((d: any) => {
+      filteredData = filteredData.filter((d: HiorEnriched) => {
         return d.area === filter.area;
       });
     }
 
     if (filter.query) {
       const re = new RegExp(filter.query, "gim");
-      filteredData = filteredData.filter((d: any) => {
+      filteredData = filteredData.filter((d: HiorEnriched) => {
         const found = d.text.match(re);
         return found && found[0];
       });
 
-      filteredData = filteredData.filter((d: any) => {
+      filteredData = filteredData.filter((d: HiorEnriched) => {
         const found = d.description.match(re);
         return found && found[0];
       });
