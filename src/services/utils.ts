f5e83aed771d530dd058932f80555cc0fb2d2df4
Jasper Swart <jasper.g.swart@gmail.com>
2022-05-12 21:22:04 +0200

    fixed constants

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,14 +1,3 @@
-import axios from "axios";
-
-export const getByUri = (uri: string) => axios.get(uri);
-
-export const getCount = (filteredItems: any, group: string, value: string): number => {
-  if (!filteredItems) {
-    return 0;
-  }
-  return filteredItems?.filter((item: any) => item[group] === value).length;
-};
-
 export function sortAsc(a: string, b: string) {
   return a < b ? -1 : a > b ? 1 : 0;
 }
