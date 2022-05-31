6921fc469329f84ee71be2deccaf419c3e0fb6bb
Jasper Swart <jasper.g.swart@gmail.com>
2022-05-31 17:59:35 +0200

    Update utils.js

--- src/test/utils.js
+++ src/test/utils.js
@@ -1,13 +1,13 @@
 import { createMemoryHistory } from "history";
 import { ThemeProvider } from "@amsterdam/asc-ui";
-import { unstable_HistoryRouter as Router } from "react-router-dom"
+import HistoryRouter from "./HistoryRouter";
 
 export const history = createMemoryHistory();
 
 export const withTheme = (Component) => {
   return (
     <ThemeProvider>
-      <Router history={history}>{Component}</Router>
+      <HistoryRouter history={history}>{Component}</HistoryRouter>
     </ThemeProvider>
   );
 };
