36240802fa83c907c0931b2388747d92cf5c862f
IanR01 <ian.r.89@gmail.com>
2023-11-06 14:01:04 +0100

    use /tmp dir

--- env.sh
+++ env.sh
@@ -3,8 +3,8 @@
 # Based on this article: https://www.freecodecamp.org/news/how-to-implement-runtime-environment-variables-with-create-react-app-docker-and-nginx-7f9d42a91d70/
 
 # Recreate config file
-rm -rf ./env-config.js
-touch ./env-config.js
+rm -rf /tmp/env-config.js
+touch /tmp/env-config.js
 
 # Add assignment 
-echo "window._env_ = { REACT_APP_APPLICATIONINSIGHTS_CONNECTION_STRING: \"${REACT_APP_APPLICATIONINSIGHTS_CONNECTION_STRING}\" }" >> ./env-config.js
+echo "window._env_ = { REACT_APP_APPLICATIONINSIGHTS_CONNECTION_STRING: \"${REACT_APP_APPLICATIONINSIGHTS_CONNECTION_STRING}\" }" >> /tmp/env-config.js
